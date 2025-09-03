ad8791f7e2229a175c1265040953109b59770df6
TheAnas04 <04anasshaikh04@gmail.com>
2025-09-03 13:44:42 +0530

    static model import

---
+++ src/modules/users/user.model.js
@@ -0,0 +1,26 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  }, {
+    tableName: "User",
+    underscored: true,
+    timestamps: true,
+  });
+
+  return User;
+}
+
